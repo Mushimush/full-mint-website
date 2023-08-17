d1674fbc796c1ec5c884c395a864fb45ce8c727b
Stanley <stanleytanyr@gmail.com>
2023-08-17 22:51:07 +0800

    MINTING FULLY WORKING

--- contracts/RoboPunksNFT.sol
+++ contracts/RoboPunksNFT.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
 
 contract RoboPunksNFT is ERC721, Ownable {
     uint256 public mintPrice;
@@ -15,16 +13,17 @@ contract RoboPunksNFT is ERC721, Ownable {
     bool public isPublicMintEnabled;
     string internal baseTokenUri;
     address payable public withdrawWallet;
-    mapping(address => uint256) walletMints;
+    mapping(address => uint256) public walletMints;
 
     constructor() payable ERC721("RoboPunks", "RP") {
         mintPrice = 0.02 ether;
         totalSupply = 0;
         maxSupply = 1000;
         maxPerWallet = 3;
+        // withrawWallet = 'wallet address'
     }
 
-    function setIsPublicMintEnabled(
+    function setIsPublicisMintEnabled(
         bool isPublicMintEnabled_
     ) external onlyOwner {
         isPublicMintEnabled = isPublicMintEnabled_;
@@ -55,7 +54,7 @@ contract RoboPunksNFT is ERC721, Ownable {
         require(success, "withdraw failed");
     }
 
-    function mint(uint256 quantity_) public payable {
+    function mint(uint quantity_) public payable {
         require(isPublicMintEnabled, "minting not enabled");
         require(msg.value == quantity_ * mintPrice, "wrong mint value");
         require(totalSupply + quantity_ <= maxSupply, "sold out");
@@ -63,6 +62,7 @@ contract RoboPunksNFT is ERC721, Ownable {
             walletMints[msg.sender] + quantity_ <= maxPerWallet,
             "exceed max wallet"
         );
+
         for (uint256 i = 0; i < quantity_; i++) {
             uint256 newTokenId = totalSupply + 1;
             totalSupply++;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
+import{ useState } from 'react';
 import './App.css';
+import MainMint from './MainMint';
+import NavBar from './NavBar';
+
 
 function App() {
-  return (
-    <div className="App">
+  const [accounts, setAccounts] = useState([]);
 
-    </div>
-  );
+  return (
+    <div className="overlay">
+    <div className="App">
+  
+  <div className="App">
+    <NavBar accounts={accounts} setAccounts={setAccounts} />
+    <MainMint accounts={accounts} setAccounts={setAccounts} />
+</div>
+<div className="moving-background"> </div>
+
+</div>
+
+  </div>);
 }
+
 
 export default App;
